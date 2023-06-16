d0a33412ffd170e2d113f22eea5d809b2f42e26f
perezfagundes <125033582+perezfagundes@users.noreply.github.com>
2023-06-16 14:27:48 +0000

    js completo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,39 +2,76 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
-@Component({
+@Component({ 
   selector: 'app-home',
-  templateUrl: 'home.page.html', 
+  templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
   imports: [IonicModule,CommonModule],
 })
 export class HomePage {
-  v1:any=null;
-  v2:any=null;
-  resultado:any=null;
-  operador:any=null;
+  //variaveis que vao ser chamada nas funcoes
+  var2=0;
+  var1=0;
+  numero='number';
+  operador='+'
+  decimal=false;
+  decimals=0;
 
+//exibir numeros e contas 
+  exibir(n: number) {
+		switch (this.numero) {
+      case 'number':
+        if (this.decimal) {
+          this.decimals++;
+          this.var2 = this.var2 + n * Math.pow(10, -this.decimals);
+        } else {
+          this.var2 = this.var2 * 10 + n;
+        }
+        break;
+      case 'operator':
+        this.var2 = n;
+        this.numero = 'number';
+        break;
+      case 'result':
+        this.var1 = 0;
+        this.var2 = n;
+        this.numero = 'number';
+    }
+  }
+  //calculo das contas matematicas 
+  operacao(o: string) {
+    // console.log('clickOperator inicio');
+    this.mostrar();
+    this.operador = o
+    this.var1 = this.var2;
+    this.numero = 'operator';
+    // console.log('clickOperator fin');
+  }
+//exibicao do resultado das contas 
+  mostrar(){
+    this.var2 = eval('' + this.var1 + this.operador + '(' + this.var2 + ')');
+    this.var1 = 0;
+    this.numero = 'result';
+    this.operador = '+';
+    this.decimal = false;
+    this.decimals = 0;
+  }
+//apagar erros e contas erradas 
+  apagar() {
+    this.var2 = 0;
+    this.var1 = 0;
+    this.numero = 'number';
+    this.decimal = false;
+    this.decimals = 0;
+  }
+	}
+
+  
 
  
-	exibir(numero:any){
-   if(this.v1==null){
-     this.v1=numero;
-   }
-   else{
-    if(this.v2==null){
-      this.v2=numero;
 
-    }
-   }
-   console.log(this.v1);
-   console.log(this.v2);
-  }
-    operacao(operador:any){
-    if(this.operador=='+'){
-      this.resultado=this.v1+this.v2;
-    }
-   }
+  
 
 
-}
+
