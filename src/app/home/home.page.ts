e4743f1730a1fb858d6f9457f7d4a5b497f9b5a7
perezfagundes <125033582+perezfagundes@users.noreply.github.com>
2023-06-16 13:15:20 +0000

    subindo js

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,13 +4,37 @@ import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
+  templateUrl: 'home.page.html', 
   styleUrls: ['home.page.scss'],
   standalone: true,
   imports: [IonicModule,CommonModule],
 })
 export class HomePage {
-  exibir(numero: number) {
-		console.log(numero);
-	}
+  v1:any=null;
+  v2:any=null;
+  resultado:any=null;
+  operador:any=null;
+
+
+ 
+	exibir(numero:any){
+   if(this.v1==null){
+     this.v1=numero;
+   }
+   else{
+    if(this.v2==null){
+      this.v2=numero;
+
+    }
+   }
+   console.log(this.v1);
+   console.log(this.v2);
+  }
+    operacao(operador:any){
+    if(this.operador=='+'){
+      this.resultado=this.v1+this.v2;
+    }
+   }
+
+
 }
